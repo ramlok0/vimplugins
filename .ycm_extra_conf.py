7f2f6342ed3425a465f83b5e1826cd11af964b21
km000057 <km000057@brnwksmk01x.ixperta.local>
2018-10-21 14:44:58 +0200

    - moved youcompleteme config file - add setup to init all plugins txt file

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,278 @@
+import os
+import os.path
+import fnmatch
+import logging
+import ycm_core
+import re
+
+def FlagsForFile( filename ):
+  return { 'flags': [
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-ferror-limit=10000',
+    '-std=c++98',
+    '-x', 'c++',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Framework/Interfaces',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Platform_Linux/GPAL/GPALMedia/Interface',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_3rdParty/blued/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_3rdParty/blued/GPAL/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/Interface',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/VoiceEngine/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/BluetoothService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/Common/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/ToneGenerationService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/MediaControlService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/SRTP/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/MediaProcessingService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Interfaces',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Platform_Linux/Interfaces',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/TestJournalService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/Common/CanonicalDialSettings/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFAAdaptor/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/SIP/Common/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/SIP/CallControl/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/SIP/FunctionalInterface/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/SIP/TeamServices/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/SIP/Signalling/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/Common/XmlSaxParser/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/Common/InterfaceProxy/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/Common/MessageExtender/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/Common/Mapper/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/Common/Kicker/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/Common/sosal/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/DisplayHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/AudioHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/CallControl/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/LocalFeatureHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/CallLogHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/FeatureHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/HFA/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/ConfigManager/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/MenuHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/CTCHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/DeviceHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/HFA/CCHandler/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CommunicationsService/Adaptor/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/AnsibleService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CSTAParser/xmlelement/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CSTAParser/cstaXML/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CSTATestService/SIP/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/OBEXService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/WSIService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/AnalyticsService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/DigitAnalysisService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/InstrumentationService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/JournalService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/EAS/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/DirectoryService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/BroadsoftService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/SidecarService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/LDAPService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CSTA_Service/CSTAComputingFunction/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/CSTA_Service/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/UCService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/PotService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/BackupService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/TeamService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/EWS/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Business_Services/MediaRecordingService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/Media/MediaProcessingService/include',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_3rdParty/SipStack/v4_1/M5T/M5TFramework/Sources/',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_3rdParty/SipStack/v4_1/M5T/M5TConfig/SIP/'
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Platform_Linux/Linux_BSP/ifx-com-linux/source/user/opensource/openssl/output/armv7l_R3/usr/include/',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_3rdParty/SipStack/v4_1/M5T/M5TIce/Sources/IceManagement/',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Platform_Linux/GPAL/Devices/include/',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_Infrastructure_Services/VideoService/include/',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_3rdParty/SipStack/v4_1/M5T/M5TConfig/',
+    '-I', '/home/km000057/GIT/mainline/vobs/Opera_3rdParty/SipStack/v4_1/M5T/M5TStun/Sources/'
+
+    '-isystem', '/usr/include/x86_64-linux-gnu/c++/7',
+    '-isystem', '/usr/include/c++/7',
+    '-l', 'pthread'
+  ] }
+
+BASE_FLAGS = [
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-std=c++11',
+    '-x', 'c++',
+    '-isystem', '/usr/include/c++/',
+    '-isystem', '/usr/local/include',
+    '-isystem', '/usr/include',
+    '-I','/usr/lib/',
+    '-I','/usr/include/',
+    '-I','include'
+]
+BASE_FLAGSOld = [
+        '-Wall',
+        '-Wextra',
+        '-Werror',
+        '-Wno-long-long',
+        '-Wno-variadic-macros',
+        '-fexceptions',
+        '-ferror-limit=10000',
+        '-DNDEBUG',
+        '-std=c++11',
+        '-x','c++',
+        '-I','/usr/lib/',
+        '-I','/usr/include/',
+        '-I','include'
+]
+
+SOURCE_EXTENSIONS = [
+        '.cpp',
+        '.cxx',
+        '.cc',
+        '.c',
+        '.m',
+        '.mm'
+        ]
+
+SOURCE_DIRECTORIES = [
+        'src',
+        'lib'
+        ]
+
+HEADER_EXTENSIONS = [
+        '.h',
+        '.hxx',
+        '.hpp',
+        '.hh'
+        ]
+
+HEADER_DIRECTORIES = [
+        'include'
+        ]
+
+def IsHeaderFile(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in HEADER_EXTENSIONS
+
+def GetCompilationInfoForFile(database, filename):
+    if IsHeaderFile(filename):
+        basename = os.path.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            # Get info from the source files by replacing the extension.
+            replacement_file = basename + extension
+            if os.path.exists(replacement_file):
+                compilation_info = database.GetCompilationInfoForFile(replacement_file)
+                if compilation_info.compiler_flags_:
+                    return compilation_info
+            # If that wasn't successful, try replacing possible header directory with possible source directories.
+            for header_dir in HEADER_DIRECTORIES:
+                for source_dir in SOURCE_DIRECTORIES:
+                    src_file = replacement_file.replace(header_dir, source_dir)
+                    if os.path.exists(src_file):
+                        compilation_info = database.GetCompilationInfoForFile(src_file)
+                        if compilation_info.compiler_flags_:
+                            return compilation_info
+        return None
+    return database.GetCompilationInfoForFile(filename)
+
+def FindNearest(path, target, build_folder):
+    candidate = os.path.join(path, target)
+    if(os.path.isfile(candidate) or os.path.isdir(candidate)):
+        logging.info("Found nearest " + target + " at " + candidate)
+        return candidate;
+
+    parent = os.path.dirname(os.path.abspath(path));
+    if(parent == path):
+        raise RuntimeError("Could not find " + target);
+
+    if(build_folder):
+        candidate = os.path.join(parent, build_folder, target)
+        if(os.path.isfile(candidate) or os.path.isdir(candidate)):
+            logging.info("Found nearest " + target + " in build folder at " + candidate)
+            return candidate;
+
+    return FindNearest(parent, target, build_folder)
+
+def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
+    if not working_directory:
+        return list(flags)
+    new_flags = []
+    make_next_absolute = False
+    path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
+    for flag in flags:
+        new_flag = flag
+
+        if make_next_absolute:
+            make_next_absolute = False
+            if not flag.startswith('/'):
+                new_flag = os.path.join(working_directory, flag)
+
+        for path_flag in path_flags:
+            if flag == path_flag:
+                make_next_absolute = True
+                break
+
+            if flag.startswith(path_flag):
+                path = flag[ len(path_flag): ]
+                new_flag = path_flag + os.path.join(working_directory, path)
+                break
+
+        if new_flag:
+            new_flags.append(new_flag)
+    return new_flags
+
+
+def FlagsForClangComplete(root):
+    try:
+        clang_complete_path = FindNearest(root, '.clang_complete')
+        clang_complete_flags = open(clang_complete_path, 'r').read().splitlines()
+        return clang_complete_flags
+    except:
+        return None
+
+def FlagsForInclude(root):
+    try:
+        include_path = FindNearest(root, 'include')
+        flags = []
+        for dirroot, dirnames, filenames in os.walk(include_path):
+            for dir_path in dirnames:
+                real_path = os.path.join(dirroot, dir_path)
+                flags = flags + ["-I" + real_path]
+        return flags
+    except:
+        return None
+
+def FlagsForCompilationDatabase(root, filename):
+    try:
+        # Last argument of next function is the name of the build folder for
+        # out of source projects
+        compilation_db_path = FindNearest(root, 'compile_commands.json', 'build')
+        compilation_db_dir = os.path.dirname(compilation_db_path)
+        logging.info("Set compilation database directory to " + compilation_db_dir)
+        compilation_db =  ycm_core.CompilationDatabase(compilation_db_dir)
+        if not compilation_db:
+            logging.info("Compilation database file found but unable to load")
+            return None
+        compilation_info = GetCompilationInfoForFile(compilation_db, filename)
+        if not compilation_info:
+            logging.info("No compilation info for " + filename + " in compilation database")
+            return None
+        return MakeRelativePathsInFlagsAbsolute(
+                compilation_info.compiler_flags_,
+                compilation_info.compiler_working_dir_)
+    except:
+        return None
+
+def FlagsForFilex(filename):
+    root = os.path.realpath(filename);
+    compilation_db_flags = FlagsForCompilationDatabase(root, filename)
+    if compilation_db_flags:
+        final_flags = compilation_db_flags
+    else:
+        final_flags = BASE_FLAGS
+        clang_flags = FlagsForClangComplete(root)
+        if clang_flags:
+            final_flags = final_flags + clang_flags
+        include_flags = FlagsForInclude(root)
+        if include_flags:
+            final_flags = final_flags + include_flags
+    return {
+            'flags': final_flags,
+            'do_cache': True
+            }
--- vimrc
+++ vimrc
@@ -56,8 +56,13 @@ exe "cs add " . "/home/km000057/GIT/mainline/vobs/cscope.files" . " " . " "
 let g:ycm_min_num_of_chars_for_completion = 2
 let g:ycm_auto_trigger = 1
 let g:ycm_autoclose_preview_window_after_insertion = 1
+let g:ycm_collect_identifiers_from_tags_files = 1
+let g:ycm_seed_identifiers_with_syntax = 1 " Completion for programming language's keyword
+let g:ycm_complete_in_comments = 1 " Completion in comments
+let g:ycm_complete_in_strings = 1 " Completion in string
 let g:ycm_add_preview_to_completeopt = 1
-let g:ycm_global_ycm_extra_conf = '~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py'
+"let g:ycm_global_ycm_extra_conf = '~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py'
+let g:ycm_global_ycm_extra_conf = '~/.vim/bundle/.ycm_extra_conf.py'
 " turn off syntax checking
 let g:ycm_show_diagnostics_ui = 0
 let g:ycm_confirm_extra_conf = 0
